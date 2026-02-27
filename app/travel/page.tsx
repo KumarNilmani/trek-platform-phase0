5574f6fabe80d4670fda403e4f3bbbe432d05f58
Neal <nilmani.neal@gmail.com>
2026-02-28 01:41:15 +0530

    Add trek and travel routes

---
+++ app/travel/page.tsx
@@ -0,0 +1,8 @@
+export default function TravelPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Travel</h1>
+      <p>This is the Travel hub page.</p>
+    </main>
+  );
+}
---
+++ app/trek/page.tsx
@@ -0,0 +1,8 @@
+export default function TrekPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Trek</h1>
+      <p>This is the Trek hub page.</p>
+    </main>
+  );
+}
